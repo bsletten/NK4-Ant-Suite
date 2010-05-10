3915c60d5207b6a72a91f5a553d35bafa53fb984
cjcormack <devnull@localhost>
2010-05-10 18:33:59 +0100

    Added tests for connecting to tests in NK via HTTP and processing the result.

---
+++ urn.test.uk.org.onegch.netkernel.ciexperiment/uk/org/onegch/netkernel/ciexperiment/test/Test1.java
@@ -0,0 +1,11 @@
+package uk.org.onegch.netkernel.ciexperiment.test;
+
+import org.netkernel.layer0.nkf.INKFRequestContext;
+import org.netkernel.module.standard.endpoint.StandardAccessorImpl;
+
+public class Test1 extends StandardAccessorImpl {
+  @Override
+  public void onSource(INKFRequestContext aContext) throws Exception {
+    aContext.createResponseFrom(false);
+  }
+}
