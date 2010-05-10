0d5245d76c5048b1ede0cf1f375c6554ecf4f9cc
cjcormack <devnull@localhost>
2010-05-11 00:34:53 +0100

    Improved test running so only one instance of NK. TestNG suite is now automatically generated.

---
+++ testNG-nk4/uk/org/onegch/netkernel/testNG/NKTestNG.java
@@ -0,0 +1,49 @@
+package uk.org.onegch.netkernel.testNG;
+
+import java.util.Map;
+
+import org.testng.ITestListener;
+import org.testng.TestNG;
+import org.testng.TestNGCommandLineArgs;
+import org.testng.TestNGException;
+import org.testng.TestRunner;
+
+public class NKTestNG extends TestNG {
+  
+  @Override
+  public void run() {
+    NKInstance.getInstance().start();
+    super.run();
+    NKInstance.getInstance().stop();
+  }
+  
+  public static NKTestNG privateMain(String[] argv, ITestListener listener) {
+    Map arguments= checkConditions(TestNGCommandLineArgs.parseCommandLine(argv));
+    
+    NKTestNG result = new NKTestNG();
+    if (null != listener) {
+      result.addListener(listener);
+    }
+
+    result.configure(arguments);
+    try {
+      result.run();
+    }
+    catch(TestNGException ex) {
+      if (TestRunner.getVerbose() > 1) {
+        ex.printStackTrace(System.out);
+      }
+      else {
+        System.err.println("[ERROR]: " + ex.getMessage());
+      }
+      result.setStatus(HAS_FAILURE);
+    }
+    
+    return result;
+  }
+  
+  public static void main(String[] argv) {
+    NKTestNG testng = privateMain(argv, null);
+    System.exit(testng.getStatus());
+  }
+}
