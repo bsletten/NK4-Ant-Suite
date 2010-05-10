6c45ad6a1318b7726a28d3490321776d9ee61906
cjcormack <devnull@localhost>
2010-05-10 21:31:36 +0100

    Tests now defined in testng.xml

--- urn.test.uk.org.onegch.netkernel.ciexperiment/uk/org/onegch/netkernel/ciexperiment/test/Test2.java
+++ urn.test.uk.org.onegch.netkernel.ciexperiment/uk/org/onegch/netkernel/ciexperiment/test/Test2.java
@@ -3,9 +3,9 @@ package uk.org.onegch.netkernel.ciexperiment.test;
 import org.netkernel.layer0.nkf.INKFRequestContext;
 import org.netkernel.module.standard.endpoint.StandardAccessorImpl;
 
-public class Test1 extends StandardAccessorImpl {
+public class Test2 extends StandardAccessorImpl {
   @Override
   public void onSource(INKFRequestContext aContext) throws Exception {
-    aContext.createResponseFrom(true);
+    aContext.createResponseFrom(false);
   }
 }
