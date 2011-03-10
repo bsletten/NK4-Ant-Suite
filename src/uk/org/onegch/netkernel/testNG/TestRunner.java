56d0e44a38e62df48b5161896e2690ae52167b4b
Chris Cormack <chris@cormack.me.uk>
2011-03-10 13:50:59 +0000

    Updated building to use SE version of NK and added self-test ant task to generated dist

--- src/uk/org/onegch/netkernel/testNG/TestRunner.java
+++ src/uk/org/onegch/netkernel/testNG/TestRunner.java
@@ -1,13 +1,20 @@
 package uk.org.onegch.netkernel.testNG;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.StringReader;
 import java.util.List;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
@@ -27,29 +34,40 @@ public class TestRunner {
       HttpClient httpclient= new DefaultHttpClient();
       
       String backendPort= System.getProperty("netkernel.http.backend.port", "1060");
-      
+
       try {
         HttpGet httpget = new HttpGet("http://127.0.0.1:" + backendPort + "/test/exec/xml/" + identifier); 
-        
-        // Create a response handler
-        ResponseHandler<String> responseHandler = new BasicResponseHandler();
-        String responseBody = httpclient.execute(httpget, responseHandler);
-        
+
+        HttpResponse response = httpclient.execute(httpget);
+        int statusCode = response.getStatusLine().getStatusCode();
+        HttpEntity entity = response.getEntity();
+        String responseBody= EntityUtils.toString(entity);
+
+        File responseFile= new File("test-output/test-responses/" + identifier + ".xml");
+        if (!responseFile.exists()) {
+          responseFile.getParentFile().mkdirs();
+          responseFile.createNewFile();
+        }
+        FileOutputStream fos = new FileOutputStream(responseFile);
+        fos.write(responseBody.getBytes());
+        fos.close();
+
+        Assert.assertEquals(statusCode, 200, "Incorrect HTTP status code");
         SAXReader saxReader= new SAXReader();
         Document doc= saxReader.read(new StringReader(responseBody));
-        
-        XPath spaceXpath = DocumentHelper.createXPath("/testlist/results/space/text()");
+
+                                     XPath spaceXpath = DocumentHelper.createXPath("/testlist/results/space/text()");
         Node spaceNode= spaceXpath.selectSingleNode(doc);
         String space= ((Text) spaceNode).getText();
-        
+
         XPath versionXpath = DocumentHelper.createXPath("/testlist/results/version/text()");
         Node versionNode= versionXpath.selectSingleNode(doc);
         String version= ((Text) versionNode).getText();
-        
+
         XPath uriXpath = DocumentHelper.createXPath("/testlist/results/uri/text()");
         Node uriNode= uriXpath.selectSingleNode(doc);
         String uri= ((Text) uriNode).getText();
-        
+
         XPath testsXpath = DocumentHelper.createXPath("//test");
         List<? extends Node> results = testsXpath.selectNodes(doc);
         for (Node node : results) {
